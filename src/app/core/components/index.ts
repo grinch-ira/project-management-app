f1f5a35e8780da22262175733245e0789b6352cc
Pas92 <pavelburdavitsyn92@gmail.com>
2022-11-01 23:17:30 +0300

    feat: create structure for each module

---
+++ src/app/core/components/index.ts
@@ -0,0 +1,3 @@
+export * from './error-modal/error-modal.component';
+export * from './footer/footer.component';
+export * from './header/header.component';
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ErrorModalComponent } from './components/error-modal/error-modal.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HttpErrorHandlerComponent } from './services/http-error-handler/http-error-handler.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    ErrorModalComponent,
+    NotFoundComponent,
+    HttpErrorHandlerComponent,
+    WelcomeComponent
+  ],
   imports: [
     CommonModule
   ]
