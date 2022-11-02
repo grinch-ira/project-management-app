52ea833fcd74fb2ba0d413919efb6ebc99edd9c0
Yuliya Shulgan <y.shulgan@godeltech.com>
2022-11-02 12:03:04 +0100

    feat: http service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpResponseService } from '@core/services/http-response.service';
 
 @NgModule({
   declarations: [
@@ -13,8 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [HttpResponseService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
