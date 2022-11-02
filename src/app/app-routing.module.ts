bbb09fedfacce65fadaed29e4fcc2d4c9ffdecb7
Hryniuk Iryna <grinch.ira@gmail.com>
2022-11-02 12:33:28 +0300

    feat: adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth/guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'board',
+    loadChildren: () => import('./board/board.module').then(m => m.BoardModule),
+    canActivate: [AuthGuard],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { AuthPageComponent } from './pages/auth-page/auth-page.component';
+import { SharedModule } from '@shared/shared.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [
+    LoginComponent,
+    RegistrationComponent,
+    AuthPageComponent
+  ],
+  imports: [CommonModule, SharedModule, AuthRoutingModule],
 })
 export class AuthModule {}
