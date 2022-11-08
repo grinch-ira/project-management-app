36218bd2004a3646d93d7c3dfd3b6eeebae011e0
Hryniuk Iryna <grinch.ira@gmail.com>
2022-11-08 14:56:07 +0300

    feat: delete checking for a logged-in user in welcome page

--- src/app/core/pages/welcome/welcome.component.ts
+++ src/app/core/pages/welcome/welcome.component.ts
@@ -1,18 +1,8 @@
 import { Component } from '@angular/core';
-import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
-export class WelcomeComponent {
-  isLogIn = false;
-
-  isLogInBehavior = this.auth.isLogIn$.subscribe(value => {
-    this.isLogIn = value;
-    return value;
-  });
-
-  constructor(private auth: AuthService) {}
-}
+export class WelcomeComponent {}
