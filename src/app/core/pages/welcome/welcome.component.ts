20da8a63a75640f0d97a808d095be2566c7f3ee3
Hryniuk Iryna <grinch.ira@gmail.com>
2022-11-08 14:06:42 +0300

    feat: adding routing in start planning button

--- src/app/core/pages/welcome/welcome.component.ts
+++ src/app/core/pages/welcome/welcome.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
-export class WelcomeComponent {}
+export class WelcomeComponent {
+  isLogIn = false;
+
+  isLogInBehavior = this.auth.isLogIn$.subscribe(value => {
+    this.isLogIn = value;
+    return value;
+  });
+
+  constructor(private auth: AuthService) {}
+}
