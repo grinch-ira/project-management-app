5d7366464d13aa4e1ed88929b0fdd3789053ba19
Pas92 <pavelburdovitsin92@gmail.com>
2022-11-09 11:40:16 +0300

    feat: generate components for board page

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '@shared/shared.module';
+import { BoardPageComponent } from './pages/board-page/board-page.component';
+import { ColumnComponent } from './components/column/column.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BoardPageComponent,
+    ColumnComponent,
+    TaskComponent,
+  ],
   imports: [
     CommonModule,
     HttpClientModule,
+    SharedModule,
   ],
 })
 export class BoardModule {}
