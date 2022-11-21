ad460e1a7e79c48b7614b0076c6f96f1110d6736
Yuliya_Shulgan <juliette7373@gmail.com>
2022-11-21 08:51:20 +0100

    fix: comments

--- src/app/core/services/loader.service.ts
+++ src/app/core/services/loader.service.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-// How to use service:
-// 1) add (public loaderService: LoaderService) to component's constructor;
-// 2) add
-// <mat-spinner *ngIf = "loaderService.isLoading | async"></mat-spinner>
-// to component's template
-
 @Injectable({
   providedIn: 'root',
 })
