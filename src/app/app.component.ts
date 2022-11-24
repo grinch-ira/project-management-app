b736d4e307e87c2c6a394b464b8c88ff70b75bc4
Yuliya_Shulgan <juliette7373@gmail.com>
2022-11-24 09:53:42 +0100

    feat: save lang after reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { LoaderService } from '@core/services/loader.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,18 @@ export class AppComponent implements OnInit {
 
   isLoading = false;
 
-  constructor(public loaderService: LoaderService, private cdref: ChangeDetectorRef) {}
+  constructor(
+    public loaderService: LoaderService,
+    private cdref: ChangeDetectorRef,
+    public translate: TranslateService
+  ) {}
 
   ngOnInit(): void {
     this.loaderService.isLoading.subscribe(isLoad => {
       this.isLoading = isLoad;
       this.cdref.detectChanges();
     });
+    const lang = localStorage.getItem('lang') || 'en';
+    this.translate.setDefaultLang(lang);
   }
 }
