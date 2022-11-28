1f9fc7e9b9dd117dd5fcda7305ebcb9f25d3f27c
Pas92 <pavelburdovitsin92@gmail.com>
2022-11-28 12:43:11 +0300

    fix: restore snackbar and add translation for it

--- src/app/core/components/board-creation-form/board-creation-form.component.ts
+++ src/app/core/components/board-creation-form/board-creation-form.component.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Board, User } from '@core/models';
 import { HttpResponseService } from '@core/services/http-response.service';
+import { TranslateService } from '@ngx-translate/core';
 import { BoardsService, UsersService } from '@shared/services';
+import { map, Observable, switchMap } from 'rxjs';
+
+interface Translate {
+  part1: string;
+  part2: string;
+}
 
 @Component({
   selector: 'app-board-creation-form',
@@ -14,11 +22,15 @@ export class BoardCreationFormComponent implements OnInit {
 
   selectedUsers: User[] = [];
 
+  private message: string = 'test';
+
   constructor(
     private formBuilder: FormBuilder,
     private apiService: HttpResponseService,
     private boardsService: BoardsService,
-    private usersService: UsersService
+    private usersService: UsersService,
+    private snackBar: MatSnackBar,
+    private translate: TranslateService
   ) {}
 
   boardForm: FormGroup = this.formBuilder.group({
@@ -43,15 +55,52 @@ export class BoardCreationFormComponent implements OnInit {
       owner: localStorage.getItem('userId') || '',
       users: (this.boardForm.get('users')?.value as string[]) || [],
     };
-    this.apiService.createBoard(board).subscribe(res => {
-      if ((res as Board)._id) {
-        this.boardForm.reset();
-        this.boardsService.addBoard(res as Board);
-      }
-    });
+    this.apiService
+      .createBoard(board)
+      .pipe(
+        switchMap(res =>
+          this.getMessage().pipe(
+            map(translate => {
+              return {
+                res: res,
+                translate: translate,
+              };
+            })
+          )
+        )
+      )
+      .subscribe(res => {
+        if ((res.res as Board)._id) {
+          this.snackBar.open(
+            `${res.translate.part1} '${this.boardForm.get('title')?.value}' ${
+              res.translate.part2
+            }`,
+            'OK',
+            {
+              duration: 2000,
+              horizontalPosition: 'center',
+              verticalPosition: 'top',
+            }
+          );
+          this.boardForm.reset();
+          this.boardsService.addBoard(res.res as Board);
+          this.message = '';
+        }
+      });
   }
 
   getFirstUser(): string {
     return this.usersService.getName(this.boardForm.get('users')?.value?.[0]);
+  }
+
+  private getMessage(): Observable<Translate> {
+    return this.translate.getTranslation(this.translate.currentLang).pipe(
+      map(translateObj => {
+        return {
+          part1: translateObj.mainPage.messagePart1,
+          part2: translateObj.mainPage.messagePart2,
+        };
+      })
+    );
   }
 }
